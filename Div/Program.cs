45e9fa3413d9c1ae1a778100cc63fca844be57f0
«Matvey <mymayl@inbox.ru>
2023-08-08 19:23:20 +0500

    Div1

--- Div/Program.cs
+++ Div/Program.cs
@@ -1,3 +1,4 @@
-﻿double numberA = 10;
-double numberB = 4;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Rundom().Next(1, 10); //1 2 3 4 ...9
+int numberB = new Rundom().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(numberA + numberB);
